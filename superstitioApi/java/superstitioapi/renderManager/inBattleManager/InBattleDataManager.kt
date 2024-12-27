6640028c5f0562683267f2936477357e30579981
火之蔓族 <2743539045@qq.com>
2024-12-28 02:49:57 +0800

    回滚costMap，让能量减免起效

--- superstitioApi/java/superstitioapi/renderManager/inBattleManager/InBattleDataManager.kt
+++ superstitioApi/java/superstitioapi/renderManager/inBattleManager/InBattleDataManager.kt
@@ -11,7 +11,7 @@ import java.util.function.Consumer
 
 object InBattleDataManager
 {
-    val costMap: HashMap<UUID, Int> = HashMap()
+    var costMap: HashMap<UUID, Int> = HashMap()
     val subscribeManageGroups: MutableList<ISubscriber> = ArrayList()
 
     @JvmStatic
@@ -78,7 +78,7 @@ object InBattleDataManager
     private fun ResetAll()
     {
         RenderInBattle.clearAll()
-        costMap.clear()
+        costMap = HashMap()
         subscribeManageGroups.clear()
     }
 }
